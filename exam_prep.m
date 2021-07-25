e1e344bf93a1223fc517da379ec297e21b685f87
a-doering <a-doering@users.noreply.github.com>
2021-07-25 23:59:45 +0200

    Add draft of matlab script for exam

---
+++ exam_prep.m
@@ -0,0 +1,32 @@
+%% Epipolar constraint, if res_1 is all zero --> then x1, x2 are images of each other
+T = [5; 4; 1];
+R = [30;20;1];
+% Replace values in x_2, x_1
+x_2 = [20; 68/5; 1];
+x_1 = [-16; 25; -20];
+
+res_1 = x_2' * cross(T, R) * x_1
+%%
+A = [  ]
+B = [ ]
+
+%%
+g = [0 0 1 4; -1 0 0 2; 0 -1 0 3; 0 0 0 1]
+
+%% 
+m = 5
+n = 7
+P = kron(eye(n), eye(m))
+A = rand(m,n)
+res = kron(A(:), A(:))'*P(:)
+
+%res == sum(sum(A*A'))%trace(A*A')
+trace(A*A')
+trace(A*A')
+%%
+[x,y] = eig(rand(3))
+
+%% New Exam Prep
+
+A = [1,2;3,4]
+diag(A)
